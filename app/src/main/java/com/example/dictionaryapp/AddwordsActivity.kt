b785b4a464e271403b7d37718765b6fc63e4d65c
uhweeka13 <uhweka@gmail.com>
2019-09-25 11:15:33 +0100

    initial commit

---
+++ app/src/main/java/com/example/dictionaryapp/AddwordsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dictionaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddwordsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addwords)
+    }
+}
